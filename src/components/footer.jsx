9441151d472292c742ef62f821f4e2735067a7b7
Cory Benton <corybenton@gmail.com>
2023-08-28 14:58:33 -0400

    moved pics again

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,13 +2,13 @@ function Footer(){
     return(
         <div>
             <a href='https://github.com/corybenton'>
-                <img className="thumb" src='../../public/github.jpg'></img>
+                <img className="thumb" src='../../public/images/github.jpg'></img>
             </a>
             <a href='https://www.linkedin.com/in/cory-benton-4b333a22a/'>
-                <img className="thumb" src='../../public/linkedin.png'></img>
+                <img className="thumb" src='../../public/images/linkedin.png'></img>
             </a>
             <a href='https://www.facebook.com/cory.benton.90/'>
-                <img className="thumb" src='../../public/facebook.jpg'></img>
+                <img className="thumb" src='../../public/images/facebook.jpg'></img>
             </a>
         </div>
     );
