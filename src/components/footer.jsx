25dd5bad2b8d46b23fa099c2359c0d6391dcbd04
Cory Benton <corybenton@gmail.com>
2023-08-28 15:14:49 -0400

    changed file paths

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,10 +2,10 @@ function Footer(){
     return(
         <div>
             <a href='https://github.com/corybenton'>
-                <img className="thumb" src='../../public/images/github.jpg'></img>
+                <img className="thumb" src='/images/github.jpg'></img>
             </a>
             <a href='https://www.linkedin.com/in/cory-benton-4b333a22a/'>
-                <img className="thumb" src='../../public/images/linkedin.png'></img>
+                <img className="thumb" src='/images/linkedin.png'></img>
             </a>
             <a href='https://www.facebook.com/cory.benton.90/'>
                 <img className="thumb" src='/images/facebook.jpg'></img>
--- src/components/pages/about.jsx
+++ src/components/pages/about.jsx
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <div className="center"> 
             <h2>About</h2>
-            <img className='me' src='../../public/images/me.jpeg'></img>
+            <img className='me' src='/images/me.jpeg'></img>
             <p className="paragraph">I have a degree in Chemistry but am looking at changing career paths.
                 I took some coding classes in college and used those skills some in 
                 my past job and liked it. So I am looking to get into something with
