1dd91ce23a1baec08b7795aa7308c62c566c282c
Cory Benton <corybenton@gmail.com>
2023-08-28 14:40:32 -0400

    added stuff about me

--- src/components/pages/about.jsx
+++ src/components/pages/about.jsx
@@ -3,7 +3,12 @@ export default function About() {
         <div className="center"> 
             <h2>About</h2>
             <img className='me' src='../../src/assets/images/me.jpeg'></img>
-            <p>Something about me.</p>
+            <p className="paragraph">I have a degree in Chemistry but am looking at changing career paths.
+                I took some coding classes in college and used those skills some in 
+                my past job and liked it. So I am looking to get into something with
+                coding.  My interests include boardgames, softball, and reading fantasy
+                novels.
+            </p>
         </div>
     )
 }
